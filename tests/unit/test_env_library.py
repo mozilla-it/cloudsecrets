5eeb57570a2337d132f2d4c3f966086d7387ae80
Stewart Henderson <shenderson@mozilla.com>
2020-04-27 20:30:42 -0500

    Execute all pre-commit hooks on the entire codebase

--- tests/unit/test_env_library.py
+++ tests/unit/test_env_library.py
@@ -1,18 +1,18 @@
-
 import unittest
 import unittest.mock as mock
 import os
 
 from cloudsecrets.env import Secrets
 
+
 class TestEnvLibrary(unittest.TestCase):
     def test_create_secret_version(self):
         s = Secrets("")
         ver = s.version
-        s.set('FAKE','SECRET')
-        assert dict(s).get('FAKE') == 'SECRET'
-        assert s.version == str(int(ver)+1)
-        s.unset('FAKE')
-        assert 'FAKE' not in dict(s)
+        s.set("FAKE", "SECRET")
+        assert dict(s).get("FAKE") == "SECRET"
+        assert s.version == str(int(ver) + 1)
+        s.unset("FAKE")
+        assert "FAKE" not in dict(s)
         s.update()
-        assert s.version == str(int(ver)+3)
+        assert s.version == str(int(ver) + 3)
