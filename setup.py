b79d1d77eab739366b1ee4fa4e9ee2a54af5c97b
Stewart Henderson <shenderson@mozilla.com>
2020-04-30 10:05:12 -0500

    AWS Support

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     author_email="afrank@mozilla.com",
     packages=find_packages(),
     entry_points={"console_scripts": ["cloud-secrets=cloudsecrets.cli:main",],},
-    install_requires=["google-cloud-secret-manager", "boto3"],
-    extras_require={"test": ["coverage", "pytest"],},
+    install_requires=["google-cloud-secret-manager", "boto3", "moto"],
+    extras_require={"test": ["coverage", "pytest", "nose"],},
     project_urls={"Source": "https://github.com/mozilla-it/cloudsecrets",},
     test_suite="tests.unit",
 )
