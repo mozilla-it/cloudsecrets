772a72bb2e3cabf116a0093ad374be58111a0666
Stewart Henderson <shenderson@mozilla.com>
2020-05-07 10:21:40 -0500

    AWS Binary vs Secrets Improvements

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     author_email="afrank@mozilla.com",
     packages=find_packages(),
     entry_points={"console_scripts": ["cloud-secrets=cloudsecrets.cli:main",],},
-    install_requires=["google-cloud-secret-manager", "boto3", "moto"],
-    extras_require={"test": ["coverage", "pytest", "nose"],},
+    install_requires=["google-cloud-secret-manager", "boto3", "moto", "simplejson"],
+    extras_require={"test": ["coverage", "pytest", "nose", "simplejson"],},
     project_urls={"Source": "https://github.com/mozilla-it/cloudsecrets",},
     test_suite="tests.unit",
 )
--- tests/unit/test_aws_library.py
+++ tests/unit/test_aws_library.py
@@ -1,6 +1,9 @@
+import json
 import unittest
 
 import boto3
+import simplejson
+from nose.tools import assert_raises
 from six import b
 
 from cloudsecrets.aws import Secrets
@@ -31,8 +34,6 @@ class TestAWSLibrary(unittest.TestCase):
 
     @mock_secretsmanager
     def test_delete_secret(self):
-        from nose.tools import assert_raises
-
         self.connection.create_secret(Name="test-secret", SecretBinary=b("{}"))
         secrets = Secrets("test-secret", connection=self.connection)
         secrets.delete()
@@ -40,3 +41,53 @@ class TestAWSLibrary(unittest.TestCase):
 
         with assert_raises(ClientError):
             self.connection.get_secret_value(SecretId="test-secret")
+
+    @mock_secretsmanager
+    def test_unpack_response_secret_string_empty_string(self):
+        secret_id = "test-secret"
+        secret_string = ""
+        self.connection.create_secret(Name=secret_id, SecretString=secret_string)
+        secret_response = self.connection.get_secret_value(SecretId=secret_id)
+        with assert_raises(simplejson.errors.JSONDecodeError):
+            Secrets.unpack_response(secret_response)
+
+    @mock_secretsmanager
+    def test_unpack_response_secret_string_empty_dictionary(self):
+        secret_id = "test-secret"
+        secret_string = json.dumps(dict())
+        self.connection.create_secret(Name=secret_id, SecretString=secret_string)
+        secret_response = self.connection.get_secret_value(SecretId=secret_id)
+        assert json.loads(secret_string) == Secrets.unpack_response(secret_response)
+
+    @mock_secretsmanager
+    def test_unpack_response_secret_string(self):
+        secret_id = "test-secret"
+        secret_dictionary = {"A": "A", "B": "B"}
+        secret_string = json.dumps(secret_dictionary)
+        self.connection.create_secret(Name=secret_id, SecretString=secret_string)
+        secret_response = self.connection.get_secret_value(SecretId=secret_id)
+        assert json.loads(secret_string) == Secrets.unpack_response(secret_response)
+
+    @mock_secretsmanager
+    def test_unpack_response_secret_binary(self):
+        import base64
+        import simplejson as json
+
+        secret_id = "test-secret"
+        expected = {"A": "A", "B": "B"}
+        binary_secret_dictionary = {}
+        for k, v in expected.items():
+            binary_secret_dictionary[k] = base64.b64encode(v.encode("ascii"))
+        self.connection.create_secret(
+            Name=secret_id, SecretBinary=json.dumps(binary_secret_dictionary)
+        )
+        secret_response = self.connection.get_secret_value(SecretId=secret_id)
+        actual = Secrets.unpack_response(secret_response)
+        assert expected == actual
+
+    @mock_secretsmanager
+    def test__create_secret_resource(self):
+        secrets = Secrets(
+            "super-secret", connection=self.connection, create_if_not_present=True
+        )
+        assert secrets.secrets == dict()
